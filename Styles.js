5402e0f8e0e44b43d52ca7b40cd4b76c65e3ba15
jatinkumar13113-art <jatinkumar13113@gmail.com>
2026-01-28 20:30:30 +0530

    Update Styles.js

--- Styles.js
+++ Styles.js
@@ -1,1 +1,11 @@
+// Telegram popup (auto)
+setTimeout(() => {
+  document.getElementById("telegram-popup").style.display = "flex";
+}, 4000);
 
+// Close popup
+document.getElementById("close-popup").onclick = () => {
+  document.getElementById("telegram-popup").style.display = "none";
+};
+
+console.log("Jaidev Edit website loaded successfully");
